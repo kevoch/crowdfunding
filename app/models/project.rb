846fd5da1de73f5f7157adaf725a57306a01fb3c
Next <nextacademy@email.com>
2016-04-24 20:37:46 -0700

    video uploader done

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,6 @@ class Project < ActiveRecord::Base
 	has_many :transactions
 	# has_many :users, through: :transactions
 	mount_uploaders :images, ImagesUploader
+	mount_uploader :avatar, AvatarUploader
 	acts_as_votable
 end
