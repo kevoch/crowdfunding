2f3f59d0cdb13282246d7233df5131e4ebe4056a
Next <nextacademy@email.com>
2016-04-26 23:52:37 -0700

    fixing payment form

--- app/assets/javascripts/transactions.js
+++ app/assets/javascripts/transactions.js
@@ -8,15 +8,15 @@ $(document).on('ready page:load', function () {
     //Initialize tooltips
     $('.nav-tabs > li a[title]').tooltip();
     
-    //Wizard
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+    // //Wizard
+    // $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 
-        var $target = $(e.target);
+    //     var $target = $(e.target);
     
-        if ($target.parent().hasClass('disabled')) {
-            return false;
-        }
-    });
+    //     if ($target.parent().hasClass('disabled')) {
+    //         return false;
+    //     }
+    // });
 
     $(".next-step").click(function (e) {
         var $active = $('.wizard .nav-tabs li.active');
@@ -40,3 +40,46 @@ function nextTab(elem) {
 function prevTab(elem) {
     $(elem).prev().find('a[data-toggle="tab"]').click();
 }
+
+
+/*Payment Form */
+$("#paymentForm").submit(function()
+{
+var datastring = $(this).serialize();
+$.ajax({
+type: "POST",
+url: "cardProcess.php",
+data: datastring,
+dataType: "json",
+beforeSend: function(){  $("#paymentButton").val('Processing..') },
+success: function(data)
+{
+$.each(data.OrderStatus, function(i,data)
+{
+var HTML;
+if(data)
+{
+$("#paymentGrid").slideUp("slow"); 
+$("#orderInfo").fadeIn("slow");
+
+if(data.status == '1')
+{
+HTML="Order <span>"+data.orderID+"</span> has been created successfully.";
+}
+else if(data.status == '2')
+{
+HTML="Transaction has been failed, please use other card.";
+}
+else
+{
+HTML="Card number is not    valid, please use other card.";
+}
+
+$("#orderInfo").html(HTML);
+}
+});
+},
+error: function(){ alert('Network Error'); }
+});
+return false;
+});
